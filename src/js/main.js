a7f1778fc73fc39210b5f635b45524b8f64cc960
vicious7577 <102733889+vicious7577@users.noreply.github.com>
2022-05-05 23:14:43 +0300

    добавили модальное окно

---
+++ src/js/main.js
@@ -0,0 +1,22 @@
+var button = document.querySelector('#button');
+var modal = document.querySelector('#modal');
+var close = document.querySelector('#close');
+
+button.addEventListener('click', function() {
+    modal.classList.add('modal_active');
+});
+
+close.addEventListener('click', function() {
+    modal.classList.remove('modal_active');
+});
+
+
+// отсчет идет от открытия сайта, а не окна
+setTimeout(function(){
+    modal.classList.remove('modal_active');
+}, 5000);
+
+// setTimeout(function(){
+//     document.getElementById('modal').style.display = 'none';
+//   }, 5000);
+
